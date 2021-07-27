3f0693f0820a02fd421a8d494fcf11c9e16631de
ArdeliaWu <ardeliautomo@gmail.com>
2021-07-27 10:04:25 +0700

    create constraint

---
+++ my-app/src/components/home/const.js
@@ -0,0 +1,4 @@
+const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
+const REDIRECT_URL_AFTER_LOGIN = "http:%2F%2Flocalhost:3000"
+const SCOPE = "playlist-modify-private"
--- my-app/src/components/home/index.js
+++ my-app/src/components/home/index.js
@@ -4,10 +4,7 @@ import PlaylistDetail from './playlistDetail/Detail';
 import data from './playlistdata';
 import './index.css';
 
-const CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID
-const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize"
-const REDIRECT_URL_AFTER_LOGIN = "http:%2F%2Flocalhost:3000"
-const SCOPE = "playlist-modify-private"
+
 
 
 
